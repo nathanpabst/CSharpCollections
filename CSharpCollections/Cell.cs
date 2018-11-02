acddd10f124f2a7e178f42adc111b7b75cd25451
Nathan Pabst <nathan.pabst@gmail.com>
2018-11-01 20:01:31 -0500

    built a spreadsheet in code

---
+++ CSharpCollections/Cell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCollections
+{
+    class Cell
+    {
+        public string Contents { get; set; }
+        
+    }
+
+}
--- CSharpCollections/Program.cs
+++ CSharpCollections/Program.cs
@@ -6,7 +6,43 @@ namespace CSharpCollections
     {
         static void Main(string[] args)
         {
+            //ARRAYS
+            //instantiating the array...
+            //int[] lockCombination = new int[3];
+            //initializing the array...
+            int[] lockCombination = { 10, 5, 32 };
+            //retrieving an item from an array
+            int last = lockCombination[2];
+            //setting or updating an index position in the array
+            lockCombination[2] = 20;
+
+            //creating a spreadsheet (array within an array)...
+            Cell[][] sheet = new Cell[100][];
+            sheet[0] = new Cell[10];
+            for (int rowIndex = 0; rowIndex < sheet.Length; rowIndex++)
+            {
+                //create a row of 10 cells
+                sheet[rowIndex] = new Cell[10];
+                //nested loop
+                for (int colIndex = 0; colIndex < sheet[rowIndex].Length; colIndex++)
+                {
+                    sheet[rowIndex][colIndex] = new Cell();
+                }
+            }
+            foreach (Cell[] row in sheet)
+            {
+                foreach (Cell cell in row)
+                {
+                    Console.Write(cell);
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(sheet[4][7]);
+
             Console.WriteLine("Hello World!");
+            Console.Read();
         }
     }
 }
