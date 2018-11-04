2d1011fe147c2b282396bfbfa7a681e81c5ef0aa
Nathan Pabst <nathan.pabst@gmail.com>
2018-11-04 12:05:38 -0600

    added a new student while maintaining the sort method.

--- CSharpCollections/Program.cs
+++ CSharpCollections/Program.cs
@@ -15,6 +15,16 @@ namespace CSharpCollections
                 new Student() { Name = "Sally", GradeLevel = 2}
             };
             students.Sort();
+            //adding a new student while maintaining the Sort()
+            Student newStudent = new Student() { Name = "Joe", GradeLevel = 2 };
+            //find out if Joe (the new student) is in the list using BinarySearch
+            int index = students.BinarySearch(newStudent);
+            //if Joe is in the list the index will have a positive integer. Otherwise we will add him using Insert().
+            //~index is a bitwise operator. it negates the index and subtracts 1
+            if (index < 0)
+            {
+                students.Insert(~index, newStudent);
+            }
             foreach (Student student in students)
             {
                 Console.WriteLine($"{student.Name} is in grade {student.GradeLevel}");
