dffc699f1737eae3957a3b7d5acb660753ebb0bc
Nathan Pabst <nathan.pabst@gmail.com>
2018-11-04 10:43:09 -0600

    added Student class with IComparable interface for sorting

---
+++ CSharpCollections/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CSharpCollections
+{
+    //IComparable generic interface requires the CompareTo method
+    class Student : IComparable<Student>
+    {
+        public string Name { get; set; }
+        public int GradeLevel { get; set; }
+
+        //
+        public int CompareTo(Student that)
+        {
+            int result = this.Name.CompareTo(that.Name);
+
+            if (result == 0)
+            {
+                result = this.GradeLevel.CompareTo(that.GradeLevel);
+            }
+            return result;
+        }
+    }
+}
