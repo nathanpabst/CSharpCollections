64065e7d29c68bfe45e90488a0bf946784a9964e
Nathan Pabst <nathan.pabst@gmail.com>
2018-11-04 12:46:50 -0600

    added schoolRoll class with AddRange method

---
+++ CSharpCollections/SchoolRoll.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCollections
+{
+    class SchoolRoll
+    {
+        private List<Student> _students = new List<Student>();
+
+        //IEnumerable<T> exposes the enumerator..supports a simple iteration (like foreach loops) over a collection of a specified type
+        public void AddStudents(IEnumerable<Student> students)
+        {
+            _students.AddRange(students);
+        }
+    }
+}
